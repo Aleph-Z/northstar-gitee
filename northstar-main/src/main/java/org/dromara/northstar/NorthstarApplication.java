f8a69d58468abe7defe888a7bc98d8da0ab53160
Carver_007 <k995211314@139.com>
2023-11-09 23:57:14 +0800

    集成老虎证劵

--- northstar-main/src/main/java/org/dromara/northstar/NorthstarApplication.java
+++ northstar-main/src/main/java/org/dromara/northstar/NorthstarApplication.java
@@ -1,7 +1,6 @@
 package org.dromara.northstar;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
-@EnableAutoConfiguration
 @EnableJpaRepositories(basePackages = "org.dromara.northstar.data.jdbc")
 @SpringBootApplication
 public class NorthstarApplication {
